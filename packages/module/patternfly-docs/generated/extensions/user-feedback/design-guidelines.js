b19b21f65456ef124dd6fd04ae2a6fd1f25cc3bd
nicolethoen <nthoen@redhat.com>
2024-06-03 16:34:25 -0400

    update to official versions

--- packages/module/patternfly-docs/generated/extensions/user-feedback/design-guidelines.js
+++ packages/module/patternfly-docs/generated/extensions/user-feedback/design-guidelines.js
@@ -27,15 +27,15 @@ pageData.examples = {
 
 const Component = () => (
   <React.Fragment>
-    <p {...{"className":"undefinedpf-v6-c-content--p ws-p "}}>
+    <p {...{"className":"pf-v6-c-content--p ws-p "}}>
       {`Note: User feedback lives in its own package at `}
-      <PatternflyThemeLink {...{"to":"https://github.com/patternfly/react-user-feedback"}}>
+      <PatternflyThemeLink {...{"to":"https://github.com/patternfly/react-user-feedback","className":""}}>
         {`@patternfly/react-user-feedback`}
       </PatternflyThemeLink>
       {`.`}
     </p>
-    <p {...{"className":"undefinedpf-v6-c-content--p ws-p "}}>
-      <strong>
+    <p {...{"className":"pf-v6-c-content--p ws-p "}}>
+      <strong {...{"className":""}}>
         {`User feedback`}
       </strong>
       {` is a modal component that products can use to collect asynchronous feedback from users.`}
@@ -43,49 +43,49 @@ const Component = () => (
     <AutoLinkHeader {...{"id":"elements","size":"h2","className":"ws-title ws-h2"}}>
       {`Elements`}
     </AutoLinkHeader>
-    <p {...{"className":"undefinedpf-v6-c-content--p ws-p "}}>
+    <p {...{"className":"pf-v6-c-content--p ws-p "}}>
       <img src={srcImport0} width={srcImport0.width} height={srcImport0.height} {...{"alt":"Image of advanced feedback modal with annotations","className":"ws-img "}}>
       </img>
     </p>
-    <ol {...{"className":"undefinedpf-v6-c-content--ol ws-ol "}}>
-      <li {...{"className":"undefinedpf-v6-c-content--li ws-li "}}>
-        <strong>
+    <ol {...{"className":"pf-v6-c-content--ol ws-ol "}}>
+      <li {...{"className":"pf-v6-c-content--li ws-li "}}>
+        <strong {...{"className":""}}>
           {`Title:`}
         </strong>
         {` Communicates the title of the modal.`}
       </li>
-      <li {...{"className":"undefinedpf-v6-c-content--li ws-li "}}>
-        <strong>
+      <li {...{"className":"pf-v6-c-content--li ws-li "}}>
+        <strong {...{"className":""}}>
           {`Description text:`}
         </strong>
         {` Provides a short description of the form's purpose.`}
       </li>
-      <li {...{"className":"undefinedpf-v6-c-content--li ws-li "}}>
-        <strong>
+      <li {...{"className":"pf-v6-c-content--li ws-li "}}>
+        <strong {...{"className":""}}>
           {`Image:`}
         </strong>
         {` Contributes to the modal's visual design.`}
       </li>
-      <li {...{"className":"undefinedpf-v6-c-content--li ws-li "}}>
-        <strong>
+      <li {...{"className":"pf-v6-c-content--li ws-li "}}>
+        <strong {...{"className":""}}>
           {`Selectable item:`}
         </strong>
         {` Provides one or more ways that users can leave feedback.`}
       </li>
     </ol>
-    <p {...{"className":"undefinedpf-v6-c-content--p ws-p "}}>
+    <p {...{"className":"pf-v6-c-content--p ws-p "}}>
       {`Each of these elements can be customized to fit a product's needs.`}
     </p>
     <AutoLinkHeader {...{"id":"behavior","size":"h2","className":"ws-title ws-h2"}}>
       {`Behavior`}
     </AutoLinkHeader>
-    <p {...{"className":"undefinedpf-v6-c-content--p ws-p "}}>
+    <p {...{"className":"pf-v6-c-content--p ws-p "}}>
       {`The user feedback modal may be accessed in your product's help menu.`}
     </p>
-    <p {...{"className":"undefinedpf-v6-c-content--p ws-p "}}>
+    <p {...{"className":"pf-v6-c-content--p ws-p "}}>
       {`For example, you can allow users to access the modal by selecting a masthead’s help icon and choosing “Share feedback” in the dropdown menu:`}
     </p>
-    <p {...{"className":"undefinedpf-v6-c-content--p ws-p "}}>
+    <p {...{"className":"pf-v6-c-content--p ws-p "}}>
       <img src={srcImport1} width={srcImport1.width} height={srcImport1.height} {...{"alt":"Image of expanded dropdown menu with link to sharing feedback","className":"ws-img "}}>
       </img>
     </p>
@@ -95,45 +95,45 @@ const Component = () => (
     <AutoLinkHeader {...{"id":"modal-items-open-forms-within-the-modal","size":"h3","className":"ws-title ws-h3"}}>
       {`Modal items open forms within the modal`}
     </AutoLinkHeader>
-    <p {...{"className":"undefinedpf-v6-c-content--p ws-p "}}>
+    <p {...{"className":"pf-v6-c-content--p ws-p "}}>
       {`Selecting an item opens a form within the modal, which can submit data over the network.`}
     </p>
-    <p {...{"className":"undefinedpf-v6-c-content--p ws-p "}}>
+    <p {...{"className":"pf-v6-c-content--p ws-p "}}>
       {`Integrated forms can collect different kinds of feedback:`}
     </p>
-    <ul {...{"className":"undefinedpf-v6-c-content--ul ws-ul "}}>
-      <li {...{"className":"undefinedpf-v6-c-content--li ws-li "}}>
-        <p {...{"className":"undefinedpf-v6-c-content--p ws-p "}}>
+    <ul {...{"className":"pf-v6-c-content--ul ws-ul "}}>
+      <li {...{"className":"pf-v6-c-content--li ws-li "}}>
+        <p {...{"className":"pf-v6-c-content--p ws-p "}}>
           {`General feedback form`}
         </p>
-        <p {...{"className":"undefinedpf-v6-c-content--p ws-p "}}>
+        <p {...{"className":"pf-v6-c-content--p ws-p "}}>
           {`The general feedback form allows users to enter feedback into a text box and submit.`}
         </p>
-        <p {...{"className":"undefinedpf-v6-c-content--p ws-p "}}>
+        <p {...{"className":"pf-v6-c-content--p ws-p "}}>
           <img src={srcImport2} width={srcImport2.width} height={srcImport2.height} {...{"alt":"Image of a form  within a  modal to share feedback","className":"ws-img "}}>
           </img>
         </p>
       </li>
-      <li {...{"className":"undefinedpf-v6-c-content--li ws-li "}}>
-        <p {...{"className":"undefinedpf-v6-c-content--p ws-p "}}>
+      <li {...{"className":"pf-v6-c-content--li ws-li "}}>
+        <p {...{"className":"pf-v6-c-content--p ws-p "}}>
           {`Bug report form`}
         </p>
-        <p {...{"className":"undefinedpf-v6-c-content--p ws-p "}}>
+        <p {...{"className":"pf-v6-c-content--p ws-p "}}>
           {`The bug report form allows users to enter feedback into a text box and submit. Its description directs users to a URL where they can submit a support ticket for critical issues.`}
         </p>
-        <p {...{"className":"undefinedpf-v6-c-content--p ws-p "}}>
+        <p {...{"className":"pf-v6-c-content--p ws-p "}}>
           <img src={srcImport3} width={srcImport3.width} height={srcImport3.height} {...{"alt":"Image of a form within a modal to report bugs","className":"ws-img "}}>
           </img>
         </p>
       </li>
-      <li {...{"className":"undefinedpf-v6-c-content--li ws-li "}}>
-        <p {...{"className":"undefinedpf-v6-c-content--p ws-p "}}>
+      <li {...{"className":"pf-v6-c-content--li ws-li "}}>
+        <p {...{"className":"pf-v6-c-content--p ws-p "}}>
           {`User research opt-in form`}
         </p>
-        <p {...{"className":"undefinedpf-v6-c-content--p ws-p "}}>
+        <p {...{"className":"pf-v6-c-content--p ws-p "}}>
           {`The user research opt-in form allows users to enter their email address to participate in future research efforts.`}
         </p>
-        <p {...{"className":"undefinedpf-v6-c-content--p ws-p "}}>
+        <p {...{"className":"pf-v6-c-content--p ws-p "}}>
           <img src={srcImport4} width={srcImport4.width} height={srcImport4.height} {...{"alt":"Image of a form within a modal to sign up for research opportunities","className":"ws-img "}}>
           </img>
         </p>
@@ -142,10 +142,10 @@ const Component = () => (
     <AutoLinkHeader {...{"id":"modal-items-link-to-external-urls","size":"h3","className":"ws-title ws-h3"}}>
       {`Modal items link to external URLs`}
     </AutoLinkHeader>
-    <p {...{"className":"undefinedpf-v6-c-content--p ws-p "}}>
+    <p {...{"className":"pf-v6-c-content--p ws-p "}}>
       {`Selecting an item redirects users to an external URL (like a qualtrics form) that opens in a separate tab.`}
     </p>
-    <p {...{"className":"undefinedpf-v6-c-content--p ws-p "}}>
+    <p {...{"className":"pf-v6-c-content--p ws-p "}}>
       <img src={srcImport5} width={srcImport5.width} height={srcImport5.height} {...{"alt":"Image of modal with links to external forms","className":"ws-img "}}>
       </img>
     </p>
